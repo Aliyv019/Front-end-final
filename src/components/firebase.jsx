c28bcec6b8c67e738a2dea690f7ae7faca2157fd
Aliyv019 <tahiraliyev2006@gmail.com>
2024-11-25 13:21:58 +0400

    corrected permission error

--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBbZTwN_rHm3xF-pkt7_NLuj13R660hGyo",
   authDomain: "messenger-71c2f.firebaseapp.com",
   projectId: "messenger-71c2f",
-  storageBucket: "messenger-71c2f.firebasestorage.app",
+  storageBucket: "messenger-71c2f.appspot.com",
   messagingSenderId: "526135825340",
   appId: "1:526135825340:web:b7aa5defea3c4e735391b9"
 };
